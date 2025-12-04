44698156414c0790be76f7b20db1ec0de72c31f2
furkanarslan1 <furkanarslandev@gmail.com>
2025-12-04 10:23:49 +0300

    feat:add productsList

---
+++ components/ProductList.tsx
@@ -0,0 +1,84 @@
+import { ProductType } from "@/types/productsType";
+import React from "react";
+
+const products: ProductType[] = [
+  {
+    id: 1,
+    name: "Adidas ",
+    shortDescription: "Lorem ipsum dolor sit amet consectetur",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit unde soluta modi debitis. Tempora nesciunt libero nobis sint omnis maxime laboriosam id, possimus architecto nihil cumque dicta qui! Quo, illum.",
+    price: 49.9,
+    sizes: ["s", "m", "l", "xl", "xxl"],
+    colors: ["gray", "purple", "green"],
+    images: {
+      gray: "/products/1g.png",
+      purple: "products/1p.png",
+      green: "products/1gre.png",
+    },
+  },
+  {
+    id: 1,
+    name: "Puma ",
+    shortDescription: "Lorem ipsum dolor sit amet consectetur",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit unde soluta modi debitis. Tempora nesciunt libero nobis sint omnis maxime laboriosam id, possimus architecto nihil cumque dicta qui! Quo, illum.",
+    price: 459.9,
+    sizes: ["s", "m", "l", "xl", "xxl"],
+    colors: ["gray", "purple", "green"],
+    images: {
+      gray: "/products/1g.png",
+      purple: "products/1p.png",
+      green: "products/1gre.png",
+    },
+  },
+  {
+    id: 1,
+    name: "Nike ",
+    shortDescription: "Lorem ipsum dolor sit amet consectetur",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit unde soluta modi debitis. Tempora nesciunt libero nobis sint omnis maxime laboriosam id, possimus architecto nihil cumque dicta qui! Quo, illum.",
+    price: 39.9,
+    sizes: ["s", "m", "l", "xl", "xxl"],
+    colors: ["gray", "green"],
+    images: {
+      gray: "/products/3g.png",
+
+      green: "products/3gre.png",
+    },
+  },
+  {
+    id: 1,
+    name: "Adidas ",
+    shortDescription: "Lorem ipsum dolor sit amet consectetur",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit unde soluta modi debitis. Tempora nesciunt libero nobis sint omnis maxime laboriosam id, possimus architecto nihil cumque dicta qui! Quo, illum.",
+    price: 49.9,
+    sizes: ["s", "m", "l", "xl", "xxl"],
+    colors: ["gray", "purple", "green"],
+    images: {
+      gray: "/products/1g.png",
+      purple: "products/1p.png",
+      green: "products/1gre.png",
+    },
+  },
+  {
+    id: 1,
+    name: "Adidas ",
+    shortDescription: "Lorem ipsum dolor sit amet consectetur",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit unde soluta modi debitis. Tempora nesciunt libero nobis sint omnis maxime laboriosam id, possimus architecto nihil cumque dicta qui! Quo, illum.",
+    price: 49.9,
+    sizes: ["s", "m", "l", "xl", "xxl"],
+    colors: ["gray", "purple", "green"],
+    images: {
+      gray: "/products/1g.png",
+      purple: "products/1p.png",
+      green: "products/1gre.png",
+    },
+  },
+];
+
+export default function ProductList() {
+  return <div>L</div>;
+}
